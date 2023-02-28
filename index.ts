142f022ae69e0e945a8fe47c7579b82190d786d6
Jouni Juntunen <jouni.juntunen@oamk.fi>
2023-02-28 12:25:30 +0200

    Finished example.

---
+++ index.ts
@@ -0,0 +1,71 @@
+import express, { Express, Request, Response } from 'express'
+import cors from 'cors' 
+import { Pool, QueryResult } from 'pg'
+
+const app: Express = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+const port = 3001
+
+app.get('/',(req: Request,res: Response) => {
+    const pool = openDb()
+    
+    pool.query('select * from task', (error:Error,result: QueryResult) => {
+        if (error) {
+            res.status(500).json({error: error.message})
+        }
+        res.status(200).json(result.rows)
+    })
+})
+
+app.post('/new',(req: Request,res: Response) => {
+    const pool = openDb()
+
+    pool.query('insert into task (description) values ($1) returning *',
+    [req.body.description], 
+    (error: Error,result: QueryResult) => {
+        if (error) {
+            res.status(500).json({error: error.message})
+        }
+      
+        res.status(200).json({id: result.rows[0].id})
+    }) 
+})
+
+
+app.delete("/delete/:id",async(req: Request,res: Response) => {
+    const pool = openDb()
+
+    const id = parseInt(req.params.id)  
+
+    pool.query('delete from task where id = $1',
+    [id], 
+    (error: Error,result: QueryResult) => {
+        if (error) {
+            res.status(500).json({error: error.message})
+        }
+      
+        res.status(200).json({id: id})
+    }) 
+})
+
+const openDb = (): Pool => {
+    const pool: Pool = new Pool ({
+/*         user: 'postgres',
+        host: 'localhost',
+        database: 'todo',
+        password: 'root',
+        port: 5435 */
+        user: 'root',
+        host: 'dpg-cfut0c9mbjsj9amjpucg-a.oregon-postgres.render.com',
+        database: 'todo_1bob',
+        password: '8fs76r7K1bSNwk1npEyQiS2Sbe4UIwkl',
+        port: 5432,
+        ssl: true 
+    })
+    return pool
+}
+
+app.listen(port)
